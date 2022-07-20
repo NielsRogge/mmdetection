43e85e7ea228cf9065dd27c352e1f3b25a052855
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2022-07-20 13:50:29 +0200

    Add single scale instead of multiscale

--- configs/convnext/mask_rcnn_convnext-t_p4_w7_fpn_fp16_ms-crop_3x_coco.py
+++ configs/convnext/mask_rcnn_convnext-t_p4_w7_fpn_fp16_ms-crop_3x_coco.py
@@ -31,6 +31,7 @@ train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
     dict(type='RandomFlip', flip_ratio=0.0),
+    dict(type='Resize', img_scale=(800, 1333), keep_ratio=True),
     # dict(
     #     type='AutoAugment',
     #     policies=[[
